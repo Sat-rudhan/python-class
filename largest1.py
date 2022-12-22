4797330c78ff8c77933e677be655d594fd2aef5f
Satrudhan <kushwahasatrudhan9843@gmail.comgit config --global user>
2022-12-22 07:09:59 +0545

    New Print fuction is added

---
+++ largest1.py
@@ -0,0 +1,14 @@
+a = input("Enter the 1st number:")
+b = input("Enter the 2nd number:")
+c = input("Enter the 3rd number:")
+
+if (a > b) and (a > c) :
+    print(f"{a} is greater than {b} and {c}")
+
+elif (b > a) and (b > c):
+    print(f"{b} is greater than {a} and {c}")
+
+else:
+    print(f"{c} is greater than {a} and {b}")
+
+print(f"New Print function is added")
